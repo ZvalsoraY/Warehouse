6c44df0b7189977e474f39c570787d589b220d62
ZvalsoraY <yazamail@yandex.ru>
2023-08-15 23:23:01 +0300

    add controllers

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse.Data;
+using Warehouse.Models;
+
+namespace Warehouse.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _db.Product;
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
